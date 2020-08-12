f20d8655bf982207ec576ae839bf9f843d20d6bf
Damian Czarnota <godevdamian@gmail.com>
2020-08-12 13:27:36 +0200

    delete SearchButton

--- src/App.js
+++ src/App.js
@@ -52,24 +52,6 @@ padding: 5px;
 outline: none;
 
 `
-const SearchButton = styled.input`
-display: block;
-padding: 10px 20px;
-background-color: red;
-color: white;
-font-weight: bold;
-border-radius: 10px;
-margin: 5px;
-cursor: pointer;
-border: .5px solid transparent;
-transition: .3s ease-in;
-outline: none;
-&:hover{
-    background: white;
-    color: red;
-    border: .5px solid red;
-}
-`
 
 function App() {
   
